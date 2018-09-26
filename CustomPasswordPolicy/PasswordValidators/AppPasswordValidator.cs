b9ba2c32c4990e557ed9b34b9fa607b95b70b1c3
Bhathiya Dasanayake <bhathiyamadusanka@gmail.com>
2018-09-26 14:27:56 +0530

    Add project files.

---
+++ CustomPasswordPolicy/PasswordValidators/AppPasswordValidator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomPasswordPolicy.PasswordValidators
+{
+    public class AppPasswordValidator : IPasswordValidator<IdentityUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<IdentityUser> manager, IdentityUser user, string password)
+        {
+            if (string.Equals(user.UserName, password, StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError
+                {
+                    Code = "UsernameAsPassword",
+                    Description = "You cannot use your username as your password"
+                }));
+            }
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
